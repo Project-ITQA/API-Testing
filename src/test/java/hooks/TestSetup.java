4d29ee31612dc23b0b3329f796825813b18cd964
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2024-12-26 00:59:27 +0530

    fixed test setup hook

--- src/test/java/hooks/TestSetup.java
+++ src/test/java/hooks/TestSetup.java
@@ -1,34 +1,41 @@
 package hooks;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
+
+import java.io.File;
 import java.io.IOException;
 
 public class TestSetup {
     private static Process process;
 
-    @BeforeClass
+    @BeforeAll
     public static void startDemoApi() {
         try {
+            System.out.println("[Demo API] =====> JAR starting.");
             ProcessBuilder processBuilder = new ProcessBuilder(
                     "java", "-jar", "libs/demo-0.0.1-SNAPSHOT.jar"
             );
-            processBuilder.redirectErrorStream(true);
+
+            File outputFile = new File("demo-api-output.log");
+            processBuilder.redirectOutput(outputFile);
             process = processBuilder.start();
 
-            Thread.sleep(5000);
-            System.out.println("Demo API JAR started successfully.");
+            Thread.sleep(6000);
+            System.out.println("[Demo API] =====> JAR started successfully.");
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to start Demo API JAR.", e);
         }
     }
 
-    @AfterClass
+    @AfterAll
     public static void stopDemoApi() {
+        System.out.println("[Demo API] =====> JAR stopping.");
         try{
             if (process != null) {
                 process.destroy();
+                System.out.println("[Demo API] =====> JAR stopped successfully.");
             }
         }
         catch (Exception e) {
