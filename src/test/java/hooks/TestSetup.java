143d25933fb8d231734169ac173bc1e1eae62190
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2024-12-25 12:50:27 +0530

    updated hooks

--- src/test/java/hooks/TestSetup.java
+++ src/test/java/hooks/TestSetup.java
@@ -1,9 +1,12 @@
 package hooks;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import java.io.IOException;
 
 public class TestSetup {
+    private static Process process;
+
     @BeforeClass
     public static void startDemoApi() {
         try {
@@ -11,13 +14,26 @@ public class TestSetup {
                     "java", "-jar", "libs/demo-0.0.1-SNAPSHOT.jar"
             );
             processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
+            process = processBuilder.start();
 
             Thread.sleep(5000);
             System.out.println("Demo API JAR started successfully.");
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to start Demo API JAR.", e);
+        }
+    }
+
+    @AfterClass
+    public static void stopDemoApi() {
+        try{
+            if (process != null) {
+                process.destroy();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to stop Demo API JAR.", e);
         }
     }
 }
