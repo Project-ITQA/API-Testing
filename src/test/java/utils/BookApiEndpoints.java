480268d72909383d86b5b78f004bfca90481d03e
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2024-12-26 00:57:23 +0530

    defined api endpoints interface

---
+++ src/test/java/utils/BookApiEndpoints.java
@@ -0,0 +1,11 @@
+package utils;
+
+public interface BookApiEndpoints {
+    String BASE_URL = "http://localhost:7081/api";
+
+    String GET_ALL = BASE_URL + "/books/";
+    String GET_BY_ID = BASE_URL + "/books/{id}";
+    String CREATE = BASE_URL + "/";
+    String UPDATE = BASE_URL + "/";
+    String DELETE = BASE_URL + "/books/{id}";
+}
