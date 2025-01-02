217e45bd296bfa87a1fb66eadc306ea70cbb089c
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2025-01-03 02:13:13 +0530

    added login step definition to base api actions

--- src/test/java/tests/BaseBookApiActions.java
+++ src/test/java/tests/BaseBookApiActions.java
@@ -1,12 +1,18 @@
 package tests;
 
 import clients.BookApiClient;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import net.serenitybdd.annotations.Steps;
 
 public class BaseBookApiActions {
     @Steps
     BookApiClient apiClient;
+
+    @Given("the user is logged in with username: {string} and password: {string}")
+    public void theUserIsLoggedInWithUsernameAndPassword(String username, String password) {
+        apiClient.authenticate(username, password);
+    }
 
     @Then("the response status code should be {int}")
     public void theResponseStatusCodeShouldBe(int statusCode) {
