626b256168d0b377bd9257e4a8c9c3a39331b8fe
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2025-01-03 00:03:55 +0530

    updated endpoints

--- src/test/java/utils/BookApiEndpoints.java
+++ src/test/java/utils/BookApiEndpoints.java
@@ -6,6 +6,6 @@ public interface BookApiEndpoints {
     String GET_ALL = BASE_URL + "/books/";
     String GET_BY_ID = BASE_URL + "/books/{id}";
     String CREATE = BASE_URL + "/books";
-    String UPDATE = BASE_URL + "/";
+    String UPDATE = BASE_URL + "/books/{id}";
     String DELETE = BASE_URL + "/books/{id}";
 }
